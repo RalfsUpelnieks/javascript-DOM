15b5599451dd24b0a685e94129d15bd33067559e
Ralfs Upelnieks <ralfs.upelnieks@gmail.com>
2021-03-16 16:03:45 +0200

    pievienots 06 uzdevums

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,11 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+    const input = document.getElementById('full_name')
+
+    input.addEventListener('input', function(e){
+        document.getElementById('greeting').innerText = "Hello there, " + input.value + "!"
+        
+    });
+    
+})
