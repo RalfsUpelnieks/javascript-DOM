943dd24af7f5162add700ed6fedaec87db28b1da
Ralfs Upelnieks <ralfs.upelnieks@gmail.com>
2021-03-16 15:38:15 +0200

    pievienots 04 uzdevums

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,23 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    const toggleButton = document.getElementById('button')
+
+    toggleButton.addEventListener('click', function(e){
+        var toggleMeCass = document.getElementsByClassName('toggle_me')
+        if(toggleMeCass[0].style.display == 'none'){
+            for (i = 0; i < toggleMeCass.length; i++) {
+                toggleMeCass[i].style.display = "inline";
+            }
+        }
+        else{
+            for (i = 0; i < toggleMeCass.length; i++) {
+                toggleMeCass[i].style.display = "none";
+                
+            }
+        }
+            
+        
+        
+    });
+    
+})
