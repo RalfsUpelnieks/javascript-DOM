ae3dcb042747a40488574785309a02183cb05567
Ralfs Upelnieks <ralfs.upelnieks@gmail.com>
2021-03-16 15:52:12 +0200

    pievienots 05 uzdevums

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,23 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    const toggleButton = document.getElementById('toggle_button')
+
+    toggleButton.addEventListener('click', function(e){
+        var toggleMeCass = document.querySelectorAll(".second_five li"); 
+        if(toggleMeCass[0].style.display == 'none'){
+            for (i = 0; i < toggleMeCass.length; i++) {
+                toggleMeCass[i].style.display = "list-item";
+            }
+        }
+        else{
+            for (i = 0; i < toggleMeCass.length; i++) {
+                toggleMeCass[i].style.display = "none";
+                
+            }
+        }
+            
+        
+        
+    });
+    
+})
