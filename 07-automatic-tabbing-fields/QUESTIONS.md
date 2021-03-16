2b33609734ba6628e886af8e1b46eee4e65f2767
Ralfs Upelnieks <ralfs.upelnieks@gmail.com>
2021-03-16 16:30:39 +0200

    pievienots 07 uzdevums

--- 07-automatic-tabbing-fields/QUESTIONS.md
+++ 07-automatic-tabbing-fields/QUESTIONS.md
@@ -7,3 +7,4 @@
 > What are some refactoring techniques you might apply to the provided solution?
 
 Your reply here...
+Es izmantoju if funkciju, lai pārbaudītu input laukā vai ir maksīmālais garuma tekst un ja ir izvēlējos nākošo input lauciņu.
--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,19 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+    const input1 = document.getElementById('first')
+    const input2 = document.getElementById('second')
+    const input3 = document.getElementById('third')
+
+    input1.addEventListener('input', function(e){
+        if(input1.value.length == input1.maxLength){
+            input2.focus()
+        }
+    });
+    input2.addEventListener('input', function(e){
+        if(input2.value.length == input2.maxLength){
+            input3.focus()
+        }
+    });
+    
+})
